011b188a645110d4ac865e839d2d4dad86d211a6
Daniel Calvin <arockne2003@yahoo.com>
2021-08-05 10:53:51 -0700

    Refactor and remove comments

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-// on page load, render  a list of already registered dogs
-//the dogs should be put on a table as a table row
-  /* <tr><td>Dog *Name*</td> <td>*Dog Breed*</td> <td>*Dog Sex*</td> <td><button>Edit</button></td></tr> */
-
-//make a dog editable
-  //click on the edit button next to a dog should populate the top form with that dog's current information
-  //on a submit
-    //a patch request should be made
-      //to update dog information
-
-//once form is submitted, the table should reflect the updated dog information.
-  //once the dog has been updated
-  //rerender the dog information
-
-
 function renderDogs() {
   fetch('http://localhost:3000/dogs')
   .then(resp => resp.json())
@@ -26,22 +11,18 @@ function registerDogs(dogs) {
 function createDog(dog) {
   const name = document.createElement('td');
   name.textContent = dog.name;
-  name.className = 'name';
 
   const breed = document.createElement('td');
   breed.textContent = dog.breed;
-  breed.className = 'breed';
 
   const sex = document.createElement('td');
   sex.textContent = dog.sex;
-  sex.className = 'sex';
 
   const btn = document.createElement('button');
   btn.textContent = 'Edit Dog';
   btn.addEventListener('click', () => dogToEdit(dog));
 
   const tr = document.createElement('tr');
-  tr.id = dog.id;
   tr.append(name, breed, sex, btn);
   
   document.querySelector('#table-body').appendChild(tr);
